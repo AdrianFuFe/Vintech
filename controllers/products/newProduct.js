95156591cbc657216e0fdf684514636453f7f796
anxogcd <anxogcd@gmail.com>
2021-05-14 18:52:34 +0200

    newProduct completo, validAuth en progreso

--- controllers/products/newProduct.js
+++ controllers/products/newProduct.js
@@ -4,17 +4,21 @@ async function newProduct(req, res, next) {
   let connection;
   try {
     connection = await getConnection();
-    const { title, price, description, ubication } = req.body;
+    const { title, price, description, ubication, category } = req.body;
+
+    if (!title || !price)
+      throw new Error("Los campos título y precio son obligatorios");
+
     const [result] = await connection.query(
       `
-        INSERT INTO products(title, price, description, ubication, modification_date)
-        VALUES(?, ?, ?, ?, ?)`,
-      [title, price, description, ubication, new Date()]
+        INSERT INTO products(id_seller, title, price, description, ubication, category, status)
+        VALUES(?, ?, ?, ?, ?, ?, ?)`,
+      [req.auth.id, title, price, description, ubication, category, "active"]
     );
 
     res.send({
-      status: "pending",
-      message: "Nueva entrada en proceso de programación",
+      status: "OK",
+      message: "Nueva entrada creada con éxito",
       id: result.insertId,
     });
   } catch (error) {
--- middlewares/validAuth.js
+++ middlewares/validAuth.js
@@ -1,11 +1,27 @@
+const { getConnection } = require("../db");
+
 async function validAuth(req, res, next) {
+  let connection;
   try {
-    console.log("validAuth sen código");
+    connection = await getConnection();
+    const { authorization } = req.headers;
+
+    if (!authorization) throw new Error("La petición debe incluír un token");
+
+    //estas dos líneas de abajo cambian con la existencia de token
+    let tokenInfo = {};
+    tokenInfo.id = authorization;
+    //COMPROBAR QUE EL TOKEN ES VÁLIDO Y DECODIFICARLO
+
+    //COMPROBAR QUE EL USUARIO EXISTA EN LA BASE DE DATOS
+
+    req.auth = tokenInfo;
+
     next();
   } catch (error) {
     next(error);
   } finally {
-    console.log("acaba validAuth");
+    if (connection) connection.release();
   }
 }
 
